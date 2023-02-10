89ac0a31fbe77f0d837b0ac90c143668ed7783ff
Saurabh Kumar Singh <74128940+100rbrajpuT@users.noreply.github.com>
2023-02-10 15:34:27 +0530

    1st commit

---
+++ Algomojo/api.js
@@ -0,0 +1,31 @@
+bar = document.querySelector(".flex_one").querySelectorAll("a");
+console.log(bar);
+bar.forEach(element =>{
+  element.addEventListener("click", function(){
+    bar.forEach(nav => nav.classList.remove("active"))
+    // bar.forEach(nav => nav.classList.remove("activa"))
+
+
+    this.classList.add("active")
+  })
+})
+
+// barr = document.querySelector(".nav-link").querySelectorAll(".active");
+// console.log(barr);
+// barr.forEach(element =>{
+//   element.addEventListener("click", function(){
+//     barr.forEach(nav => nav.classList.remove("active"))
+    
+
+
+//     this.classList.add("activa")
+//   })
+// })
+
+// function ChangeColor(){
+  
+// var arrow = document.querySelector(".activa")
+// arrow.style.color = "white"
+// arrow.style.color = "blue"
+// }
+
